6fa40efe79c5bbf8a0d746e9b75292e9f8c0e69f
Biswajit Routray <biswajit.routray@nettantra.net>
2024-05-12 20:01:40 +0530

    updated

--- src/app/(with-nav-bar)/page.tsx
+++ src/app/(with-nav-bar)/page.tsx
@@ -3,7 +3,7 @@ export default  function Home() {
 
   return (
     <section className="h-screen w-screen">
-      <div className="shadow-custom h-10 p-10 m-10 mt-96">Home</div>
+      <div className="shadow-custom h-10 p-10 m-10 mt-96">bjbdjsbdjsbdjsbdsjdbsjdb</div>
     </section>
   );  
 }
