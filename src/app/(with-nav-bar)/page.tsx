469213832654017ce5983b288a4f43ebc62dcb79
Biswajit Routray <biswajit.routray@nettantra.net>
2024-05-12 20:12:20 +0530

    added the redirection

--- src/app/(with-nav-bar)/page.tsx
+++ src/app/(with-nav-bar)/page.tsx
@@ -1,9 +1,11 @@
+import { redirect } from "next/navigation";
 
-export default  function Home() {
+
+export default function Home() {
 
   return (
     <section className="h-screen w-screen">
-      <div className="shadow-custom h-10 p-10 m-10 mt-96">bjbdjsbdjsbdjsbdsjdbsjdb</div>
+      <div onClick={()=>redirect("/about")} className="shadow-custom h-10 p-10 m-10 mt-96">Click on me</div>
     </section>
   );  
 }
