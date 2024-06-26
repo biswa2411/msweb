fb7a9aae39e81dbe6f682c3e1512f524443077f7
Biswajit Routray <biswajit.routray@nettantra.net>
2024-06-26 13:09:05 +0530

    auth card updated

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -8,9 +8,10 @@ export default function DashboardLayout({
   return (
 
     <div className="bg-primary w-full h-screen flex justify-between px-20 items-center">
-      <div className="flex flex-col w-full justify-center items-center gap-5  ">
+     <div className="w-full  justify-center items-center flex">
+     <div className="flex bg-primary flex-col w-fit p-10 justify-center items-center gap-5 shadow-custom rounded-[40px] ">
         <img
-          className="w-[175px] h-[175px]  left-[calc(50%_-_446px)] top-[calc(50%_-_363px)]"
+          className="w-[175px] h-[175px]  left-[calc(50%_-_446px)] top-[calc(50%_-_363px)] shadow-custom rounded-full"
           style={{ objectFit: "cover" }}
           src="/logo.svg"
         />  
@@ -18,6 +19,7 @@ export default function DashboardLayout({
         {children}
 
       </div>
+     </div>
 
       <img
         className="rounded-[32px] w-[656px] h-[904px]  left-[724px] top-[60px]"
