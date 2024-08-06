b950dff810e20e5b586bd13c223102b9c171fb66
Shyamalendu Nayak <shyamalendu.nayak@hyscaler.com>
2024-08-06 07:48:20 +0530

    About page  hero section responsiveness added

--- src/components/pages/about/HeroSection.tsx
+++ src/components/pages/about/HeroSection.tsx
@@ -3,23 +3,22 @@ import React from "react";
 
 export const HeroSection = () => {
   return (
-    <div className="flex md:flex-row flex-col  px-20 h-[700px] bg-[#f8f8f8]">
-      <div className="w-full md:w-2/3 pr-20 flex flex-col justify-center text-start gap-10">
-        <div className="w-full  [font-family:'Poppins-SemiBoldItalic',Helvetica] font-semibold italic text-[#0e2920] text-3xl  tracking-[0] leading-[normal]">
+    <div className="flex md:flex-row flex-col px-5 md:px-10 lg:px-20 pt-5 md:pt-10 bg-[#f8f8f8]">
+      <div className="w-full md:w-2/3 md:pr-20 flex flex-col justify-center text-center md:text-start gap-10">
+        <div className="w-full pt-[10%] [font-family:'Poppins-SemiBoldItalic',Helvetica] font-semibold italic text-[#0e2920] text-[16px] md:text-[18px] lg:text-[24px]  tracking-[0] leading-[normal]">
           ABOUT US
         </div>
-        <p className="w-full [font-family:'Instagram_Sans-Bold',Helvetica] font-bold text-[#0e2920] text-[68px] tracking-[0] leading-[normal]">
+        <p className="w-full [font-family:'Instagram_Sans-Bold',Helvetica] font-bold text-[#0e2920] text-[16px] md:text-[24px] lg:text-[48px] tracking-[0] leading-[normal]">
           Welcome to MS Art Life, your gateway to a world of captivating and
           customized artworks!
         </p>
-        <p className="w-full  [font-family:'Poppins-Medium',Helvetica] font-medium text-[#0e2920] text-3xl tracking-[0] leading-[normal]">
+        <p className="w-full pb-[10%] [font-family:'Poppins-Medium',Helvetica] font-medium text-[#0e2920] text-[14px] md:text-[16px] tracking-[0] leading-[normal]">
           Your online source for unique digital paintings. Transform your space
           with our curated collection.
         </p>
       </div>
-      <div className="w-full md:w-1/3 items-end flex ">
+      <div className="w-full md:w-1/3 items-end flex justify-center ">
         <Image
-         
           height={660}
           width={593}
           alt="Rectangle"
