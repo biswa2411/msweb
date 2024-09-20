245cb6a4393aac040aeb9c0b7334ffe65142d85f
Biswajit Routray <biswajit.routray@nettantra.net>
2024-09-20 15:18:50 +0530

    imge optimisation

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,4 @@
-import WaveBg from "@components/svg/WaveBg";
-
-import BG1 from "../../../public/productInfo/img1.svg"
-
+import Image from 'next/image';
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
@@ -9,36 +6,51 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
+    <div className="bg-gradient-to-r from-amber-100 via-lime-200 to-green-200 min-h-screen flex justify-center px-16 items-center relative">
+      {/* Preloading background images */}
+      <link rel="preload" href="/productInfo/img1.svg" as="image" />
+      <link rel="preload" href="/productInfo/img2.svg" as="image" />
+      <link rel="preload" href="/productInfo/img3.svg" as="image" />
+      <link rel="preload" href="/productInfo/img4.svg" as="image" />
+      <link rel="preload" href="/productInfo/img5.svg" as="image" />
 
-    <div className="bg-gradient-to-r from-amber-100 via-lime-200 to-green-200 min-h-screen flex justify-center px-16 items-center relative ">
+      {/* Background SVGs */}
       <div className="absolute grid grid-cols-4 bg items-center h-screen w-screen pl-20">
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img1.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-2000" />
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img2.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-4000" />
-        <div className="  size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img3.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob " />
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img4.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-2000" />
-
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img5.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-4000" />
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img1.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-" />
-        <div className=" size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img1.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-2000" />
-        <div className="  size-80 bg-no-repeat bg-center bg-cover  bg-[url('/productInfo/img3.svg')]  mix-blend-multiply filter blur-md opacity-70 animate-blob animation-delay-4000" />
-
+        {[
+          '/productInfo/img1.svg',
+          '/productInfo/img2.svg',
+          '/productInfo/img3.svg',
+          '/productInfo/img4.svg',
+          '/productInfo/img5.svg',
+          '/productInfo/img1.svg',
+          '/productInfo/img1.svg',
+          '/productInfo/img3.svg',
+        ].map((url, idx) => (
+          <div
+            key={idx}
+            className={`size-80 bg-no-repeat bg-center bg-cover mix-blend-multiply filter blur-md opacity-70 animate-blob ${
+              idx % 2 === 0 ? 'animation-delay-2000' : 'animation-delay-4000'
+            }`}
+            style={{ backgroundImage: `url(${url})` }}
+          />
+        ))}
       </div>
 
-      <div className="w-full  justify-center items-center flex">
-        <div className="flex bg-primary z-10 bg-opacity-20 backdrop-blur-lg  space-x-4 flex-col w-fit p-10 justify-center items-center gap-5 shadow-custom rounded-[40px] ">
-          <img
-            className="size-[150px]  left-[calc(50%_-_446px)] top-[calc(50%_-_363px)] shadow-custom rounded-full"
-            style={{ objectFit: "cover" }}
+      {/* Main Content */}
+      <div className="w-full justify-center items-center flex">
+        <div className="flex bg-primary z-10 bg-opacity-20 backdrop-blur-lg space-x-4 flex-col w-fit p-10 justify-center items-center gap-5 shadow-custom rounded-[40px]">
+          <Image
+            width={150}
+            height={150}
+            className="shadow-custom rounded-full"
+            style={{ objectFit: 'cover' }}
             src="/logo.svg"
+            alt="Logo"
+            priority
           />
-
           {children}
-
         </div>
       </div>
-
-      {/* <WaveBg className="w-full  absolute left-0 bottom-0"/> */}
-
     </div>
   );
 }
