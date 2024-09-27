1fb28da831aa258b8349d606b3f6e9d7dc3cc6b2
Biswajit Routray <biswajit.routray@nettantra.net>
2024-09-27 12:03:01 +0530

    ga added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "@components/lib/GoogleAnalytics";
 
 
 const montserrat = Montserrat({
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${montserrat.className} `}>
       <head><link rel="icon" href="/logo.svg" sizes="any" /></head>
+      <GoogleAnalytics/>
       <body>{children}</body>
       <Analytics />
     </html>
