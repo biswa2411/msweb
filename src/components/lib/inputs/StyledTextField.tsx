b72f3054456e1cf93400ab1a9486dae6de516d8d
TUSHAR TUSHAR <biswajitroutray58@gmail.com>
2024-12-27 12:27:11 +0530

    textinput updated

--- src/components/lib/inputs/StyledTextField.tsx
+++ src/components/lib/inputs/StyledTextField.tsx
@@ -21,76 +21,78 @@ const StyledTextField = (props: any) => {
 
     const defaultSx: SxProps<Theme> = {
         '& .MuiOutlinedInput-root': {
-            // backgroundColor: '#FFF3E3', // Background color of the input field
             '& fieldset': {
                 borderColor: '#2F463E',
-                borderWidth: '0.1', // Default border color
+                borderWidth: '0.1',
             },
             '&:hover fieldset': {
-                borderColor: '#2F463E', // Border color on hover
+                borderColor: '#2F463E',
             },
             '&.Mui-focused fieldset': {
-                borderColor: '#B88E2F', // Border color when focused
+                borderColor: '#B88E2F',
             },
             '& input': {
                 color: '#2F463E', // Input text color
                 fontWeight: 'bold',
-                fontSize: "18px"
+                fontSize: '13px', // Set text size to 12px
+                alignItems:"center"
             },
         },
         '& .MuiInputLabel-root': {
-            color: '#2F463E', // Label color when input is blank
+            color: '#2F463E', // Label color
             fontWeight: 'bold',
+            fontSize: '12px', // Set label text size to 12px
             '&.Mui-focused': {
-                color: '#2F463E', // Label color when focused
+                color: '#2F463E',
                 fontWeight: 'bold',
-
             },
         },
         '& .MuiInputLabel-shrink': {
-            // fontSize: '30px', // Label font size when shrink
-            color: "#1E342d",
-
-            fontWeight: 'bold', // Label font weight when shrink
+            color: 'red',
+            fontWeight: 'bold',
         },
-
     };
-
+    
     return (
-        type !== 'password' ? <TextField
-
-            size={"small"}
-            variant="outlined"
-
-            type={type === "password" && showPassword ? 'text' : type}
-            sx={{ ...defaultSx, ...sx }} // Combine default styles with any additional styles passed in
-            {...otherProps} // Spread other props to allow customizability
-        /> : <FormControl variant="outlined" sx={{ ...defaultSx, ...sx }}>
-            <InputLabel htmlFor="outlined-adornment-password">{props?.label ?? "Password"}</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                    <InputAdornment position="end">
-                        <IconButton
-                            aria-label={
-                                showPassword ? 'hide the password' : 'display the password'
-                            }
-                            onClick={handleClickShowPassword}
-                            onMouseDown={handleMouseDownPassword}
-                            onMouseUp={handleMouseUpPassword}
-                            edge="end"
-                        >
-                            {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                    </InputAdornment>
-                }
-
-                sx={{ ...defaultSx, ...sx }} // Combine default styles with any additional styles passed in
+        type !== 'password' ? (
+            <TextField
+                size="small"
+                variant="outlined"
+                type={type === "password" && showPassword ? 'text' : type}
+                sx={{ ...defaultSx, ...sx }}
                 {...otherProps}
             />
-        </FormControl>
+        ) : (
+            <FormControl variant="outlined" sx={{ ...defaultSx, ...sx }}>
+                <InputLabel htmlFor="outlined-adornment-password">
+                    {props?.label ?? "Password"}
+                </InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={showPassword ? 'text' : 'password'}
+                    style={{justifyItems:"fu"}}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label={
+                                    showPassword ? 'hide the password' : 'display the password'
+                                }
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                onMouseUp={handleMouseUpPassword}
+                                edge="end"
+                            >
+                                {showPassword ? <VisibilityOff fontSize='small'  /> : <Visibility fontSize='small' />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    sx={{ ...defaultSx, ...sx }}
+                    {...otherProps}
+                />
+            </FormControl>
+        )
     );
+    
 };
 
 export default StyledTextField;
